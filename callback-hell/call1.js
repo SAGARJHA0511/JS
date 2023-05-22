d1242044d8900ec9ac4488b3060340b94947c6b6
Sagar Jha <sagarjha0511@gmail.com>
2023-05-23 00:23:18 +0530

    ok

---
+++ callback-hell/call1.js
@@ -0,0 +1,45 @@
+// Use Of Callback Hell 
+
+
+/*console.log ("Helllo")
+
+setTimeout(function(){
+   console.log("Oggy");
+
+}, 2000 );
+var z 
+for(var i=0;i<100;i++){
+
+   console.log(i);
+
+}
+
+for (var i = 2; i <= 100; i++) {
+   if (i % 2 == 0) {
+
+      console.log(i);
+   }
+
+}
+
+for(var i = 0; i<=100; i++ ){
+   if(i%2 !=0){
+      console.log(i);
+   }
+}*/
+
+
+
+for (var i=0; i<=20; i++){
+   if(i === 0){
+      console.log(i);
+   }
+
+else if (i % 2 === 0){
+   console.log(i);
+
+}
+else{
+   console.log(i)
+}
+}
--- module2.js
+++ module2.js
@@ -3,4 +3,68 @@
 
 // ES6 modules
 
-  
+// async & await 
+
+  /*const getDataa = async()=>{
+    var getData =("Hello Oggy");
+    return getData
+  }
+//   console.log(getData);
+
+  getData().then(data => console.log(data));
+
+const getData = async()=>{
+    var x  = await  ("oggy")
+    console.log(x);
+
+}
+
+console.log(1);
+
+getData();
+
+console.log(2);*/
+
+// Implicit Binding
+
+
+// const person = {
+//     name : "oggy",
+//     age : 22,
+
+//     fan:function(){
+//         return `Hello ${this.name}your age is ${this.age} year old`
+//     }
+//  }
+
+//  console.log(person.fan());
+
+// const age = 22;
+//  function verifyAge(){
+//     return this.age
+//  }
+//   console.log(verifyAge());
+
+
+
+//   console.log(this);
+
+
+
+	// Hoisting
+
+
+	// function codeHoist(){
+	// 	a = 10;
+	// 	 let b = 50;
+	// }
+	// codeHoist();
+
+	// console.log(a); 
+    
+    //  console.log(b); 
+
+
+     console.log(name);
+     var name =190;
+     
