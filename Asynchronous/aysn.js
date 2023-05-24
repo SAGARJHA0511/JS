2a1fbe4ad59b74375c90e0b1d143b7caa616d2ea
Sagar Jha <sagarjha0511@gmail.com>
2023-05-25 00:15:08 +0530

    ok

--- Asynchronous/aysn.js
+++ Asynchronous/aysn.js
@@ -2,9 +2,16 @@ console.log("Oggy ki Dunia");
 
 setTimeout(() => {
     for (let i=0; i<30; i++){
+
         const lol =i;
         console.log("This is i number " +i);
     }
 }, 5000);
 
 console.log("Done with this ");
+
+
+
+
+
+    
