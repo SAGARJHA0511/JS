d03b6b4b5c27ca5cf7d1099e402f37f76e989195
Sagar Jha <sagarjha0511@gmail.com>
2023-05-26 00:19:20 +0530

    longestword

--- Asynchronous/aysn.js
+++ Asynchronous/aysn.js
@@ -1,17 +1,55 @@
-console.log("Oggy ki Dunia");
+// console.log("Oggy ki Dunia");
 
-setTimeout(() => {
-    for (let i=0; i<30; i++){
+// setTimeout(() => {
+//     for (let i=0; i<30; i++){
 
-        const lol =i;
-        console.log("This is i number " +i);
-    }
-}, 5000);
+//         const lol =i;
+//         console.log("This is i number " +i);
+//     }
+// }, 5000);
 
-console.log("Done with this ");
-
-
+// console.log("Done with this ");
 
 
 
+
+// for (var i=0; i<3; i++){
+//     setTimeout(function(){alert(i);},1000 + i);
     
+// }
+//     console.log(i);
+
+
+// function QuestionsMarks(str) { 
+//     res = false;
+//     for(let i=0; i<str.length; i++){
+//         for(let j=i+1; j<str.length; j++){
+//             if(Number(str[i]) + Number(str[j])=== 10){
+//                 res = true;
+//                 if(str.slice(i,j).split("?").length -1 < 3){
+//                     return false;
+//                 }
+//             }
+//         }
+//     }
+//     return res;
+// }
+   
+// // keep this function call here 
+// // QuestionsMarks(readline());
+// console.log(QuestionsMarks(""))
+
+
+function LongestWord(sen){
+    var words = sen.match(/\w+/g);
+    var longest = word[0];
+
+    for (var i=0; i<words.length; i++){
+        if (words[i].lenght> longest.lenght){
+            longest = words[i];
+
+        }
+    }
+    return longest
+}
+console.log(LongestWord("fun&!! time"));
---
+++ Asynchronous/one.js
@@ -0,0 +1,12 @@
+// const array = [1,2,3,3,4,5,5];
+
+//    const finddup = array => array.filter((gon,jiyan ) => arr.indexOf(item) !== jiyan)
+
+
+// const dupOggy = finddup(array);
+
+
+
+// console.log(dupOggy);
+
+
